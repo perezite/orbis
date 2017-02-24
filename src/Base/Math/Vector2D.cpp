5a230ff4f697c84144ba0f52ef297c972007e852
Simon Schwegler <simulo2000@gmail.com>
2017-02-24 20:54:51 +0100

    Restructuring folders to add initial Base project (WIP)

---
+++ src/Base/Math/Vector2D.cpp
@@ -0,0 +1,35 @@
+#include "Vector2D.h"
+
+namespace Math
+{
+	const Vector2D Vector2D::Zero = Vector2D(0.0f, 0.0f);
+
+	const Vector2D Vector2D::One = Vector2D(1.0f, 1.0f);
+
+	const Vector2D& Vector2D::operator=(const Vector2D &other)
+	{
+		m_x = other.m_x;
+		m_y = other.m_y;
+		return *this;
+	}
+
+	const Vector2D Vector2D::operator+(const Vector2D & other) const
+	{
+		return Vector2D(m_x + other.m_x, m_y + other.m_y);
+	}
+
+	const Vector2D Vector2D::operator-(const Vector2D &other) const
+	{
+		return Vector2D(m_x - other.m_x, m_y - other.m_y);
+	}
+
+	const Vector2D Vector2D::operator*(float scalar) const
+	{
+		return Vector2D(scalar * m_x, scalar * m_y);
+	}
+
+	const Vector2D Vector2D::operator-() const
+	{
+		return Vector2D(-m_x, -m_y);
+	}
+}
---
+++ src/Base/Math/Vector2D.h
@@ -0,0 +1,59 @@
+#pragma once
+
+namespace Math
+{
+	// Represents a 2D vector
+	class Vector2D
+	{
+	public:
+		// costructor 
+		Vector2D() { m_x = 0.0f, m_y = 0.0f; }
+
+		// constructor
+		Vector2D (float x, float y) : m_x(x), m_y(y) { }
+
+		// constructor 
+		Vector2D(const Vector2D &other) : m_x(other.m_x), m_y(other.m_y) { }
+
+		// destructor
+		virtual ~Vector2D() { };
+
+		// assignment
+		const Vector2D& operator=(const Vector2D &other);
+
+		// addition
+		const Vector2D operator+(const Vector2D &other) const;
+
+		// subtraction
+		const Vector2D operator-(const Vector2D &other) const;
+
+		// scalar multiplication
+		const Vector2D operator*(float scalar) const;
+
+		// additive inverse
+		const Vector2D operator-() const;
+
+		// get x
+		float GetX() const { return m_x; }
+
+		// get y
+		float GetY() const { return m_y; }
+
+		// set x
+		void SetX(float x) { m_x = x; }
+
+		// set y
+		void SetY(float y) { m_y = y; }
+
+	public:
+		// zero vector
+		static const Vector2D Zero;
+
+		// one vector
+		static const Vector2D One;
+
+	private:
+		float m_x;
+		float m_y;
+	};
+}
