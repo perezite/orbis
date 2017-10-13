79689c192725b9dd670a41fb4d3653f514cc945c
Simon Schwegler <simulo2000@gmail.com>
2017-10-13 22:44:15 +0200

    Fixed a merging fail

--- src/Orbis/Components/TriangleRenderer.cpp
+++ src/Orbis/Components/TriangleRenderer.cpp
@@ -25,7 +25,8 @@ namespace Components
 	{
 		Renderer* renderer = VideoManager::GetInstance()->GetRenderer();
 
-		std::vector<Vector2D> texCoords = { Vector2D(0.0f, 0.0f), Vector2D(1.0f, 0.0f), Vector2D(0.5f, 1.0f) };
+		std::vector<Vector2D> vertices = { Vector2D(-0.3f, -0.3f), Vector2D(0.3f, -0.3f), Vector2D(-0.3f, 0.3f), Vector2D(0.3f, 0.3f) };
+		std::vector<Vector2D> texCoords = { Vector2D(0.0f, 0.0f), Vector2D(1.0f, 0.0f), Vector2D(0.0f, 1.0f), Vector2D(1.0f, 1.0f) };
 		std::vector<int> indices = { 0, 1, 2, 1, 3, 2 };
 		renderer->AddGeometry(vertices, texCoords, indices);
 	}
@@ -33,7 +34,7 @@ namespace Components
 	void TriangleRenderer::Render()
 	{
 		static Renderer* renderer = VideoManager::GetInstance()->GetRenderer();
-		static const float omega = MathHelper::GetPi() * 0.0f;
+		static const float omega = MathHelper::GetPi();
 		static float alpha = 0.0f;
 
 		alpha += omega * TimeManager::GetInstance()->GetDeltaSeconds();
