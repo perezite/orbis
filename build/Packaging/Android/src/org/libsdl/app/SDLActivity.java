7039dd8295fdab2935da356a601c02acd4404fd2
Simon Schwegler <simulo2000@gmail.com>
2017-12-08 00:20:10 +0100

    Android: Added navbar hiding on resume

--- build/Packaging/Android/src/org/libsdl/app/SDLActivity.java
+++ build/Packaging/Android/src/org/libsdl/app/SDLActivity.java
@@ -179,12 +179,11 @@ public class SDLActivity extends Activity {
 		int iFlags = 
             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-			View.SYSTEM_UI_FLAG_LOW_PROFILE |
             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
             View.SYSTEM_UI_FLAG_FULLSCREEN;
 
-        getWindow().getDecorView().setSystemUiVisibility(iFlags);        
+        getWindow().getDecorView().setSystemUiVisibility(iFlags);     
         
         // Get filename from "Open with" of another application
         Intent intent = getIntent();
@@ -221,6 +220,15 @@ public class SDLActivity extends Activity {
         }
 
         SDLActivity.handleResume();
+
+			int iFlags = 
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+        getWindow().getDecorView().setSystemUiVisibility(iFlags);    
     }
 
 
