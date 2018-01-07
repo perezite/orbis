22b13ceb7a06e2a1c1224f3966326cd23a1501af
Simon Schwegler <simulo2000@gmail.com>
2018-01-07 22:08:22 +0100

    Adding coordinate system (WIP)

--- src/Application/Levels/Level8.cpp
+++ src/Application/Levels/Level8.cpp
@@ -1,8 +1,10 @@
 #include "Level8.h"
 
 #include "../../Orbis/Components/BezierCurveEditor.h"
+#include "../../Orbis/Components/SpriteRenderer.h"
 #include "../../Orbis/Components/Camera.h"
 #include "../../Orbis/Video/VideoManager.h"
+#include "../../Orbis/Video/Texture.h"
 using namespace Components;
 using namespace Video;
 
@@ -14,12 +16,20 @@ namespace Levels
 {
 	Level8::Level8()
 	{
+		// init textures
+		Texture* coordSystemTex = new Texture("Textures/CoordinateSystem2.png");
+
 		// init camera
 		Entity* cam = new Entity();
 		cam->AddComponent(new Camera());
 		AddEntity(cam);
 
-		// add bezier curve visualizer
+		// the bezier curve coordinate system
+		Entity* bccse = new Entity();
+		bccse->AddComponent(new SpriteRenderer(coordSystemTex));
+		AddEntity(bccse);
+
+		// add bezier curve editor
 		Entity* bcve = new Entity();
 		bcve->AddComponent(new BezierCurveEditor());
 		AddEntity(bcve);
