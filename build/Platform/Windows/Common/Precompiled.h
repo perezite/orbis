99bff3ef27b978e28e63437fc12150e0763db921
Simon Schwegler <simulo2000@gmail.com>
2018-06-02 13:08:05 +0200

    Added (implicit) precompiled headers for windows platform

---
+++ build/Platform/Windows/Common/Precompiled.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <limits>
+#include <math.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <memory.h>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <tuple>
+#include <stdio.h>
+#include <time.h>
+#include <map>
+#include <array>
+#include <cstdlib>
+#include <stddef.h>
+#include <set>
+
+#ifdef WIN32
+	#define NOMINMAX		// prevent redefinition of min/max by windows.h
+	#include <windows.h>
+	#include <direct.h>
+#endif
+
+#include <stdlib.h>
