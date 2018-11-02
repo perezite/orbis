266adf1e890a81c4810acf4a367eebf01007e602
Simon Schwegler <simulo2000@gmail.com>
2018-11-02 22:30:01 +0100

    Bootstrapping triangle (WIP)

--- src/Application/Main.cpp
+++ src/Application/Main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
 int main(int argc, char* args[])
-{	
-	int i = 42;
-	std::cout << "Hello World!" << std::endl;
+{
+	/*
+	orb::Mesh mesh(orb::PrimitiveType::Triangle);
+	mesh[0].vertex = orb::Vertex(orb::Vector2f(-1.0f, -1.0f, orb::Color(1, 0, 0));
+	mesh[1].vertex = orb::Vertex(orb::Vector2f( 1.0f, -1.0f, orb::Color(0, 1, 0));
+	mesh[2].vertex = orb::Vertex(orb::Vector2f( 1.0f,  0.0f, orb::Color(0, 0, 1));
+	
+	// orb::Engine::instance()->run();
+	orb::Engine()->run();			// Mal schauen, ob sogar das geht...
+	*/
 
-	std::cin.get();
 
-	return 0;
+
 }
