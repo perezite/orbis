8a5eb50cb265fa97cd5bd30a83c9bcdbb4c26f0e
Simon Schwegler <simulo2000@gmail.com>
2018-11-06 20:10:40 +0100

    Bootstrapping (wip)

--- src/application/Main.cpp
+++ src/application/Main.cpp
@@ -1,30 +1,21 @@
 #include "../orbis/Engine/Orbis.h"
 #include "../orbis/entities/Shape.h"
 #include "../orbis/core/Log.h"
+#include "levels/Level1.h"
 
-void level2()
-{
-	orb::Orbis* orbis = orb::Orbis::instance();
-	while (orbis->isLevelRunning())
-	{
-		orb::Log().message() << "level2::update" << std::endl;
-		orbis->updateFrame();
-	}
-}
-
-void level1()
-{
+void level0()
+{		
 	orb::Shape shape(3);
 	shape[0] = orb::Vertex(orb::Vector2f(-1, -1), orb::Color(255, 0, 0));
 	shape[1] = orb::Vertex(orb::Vector2f(1, -1), orb::Color(0, 255, 0));
 	shape[2] = orb::Vertex(orb::Vector2f(0, 1), orb::Color(0, 0, 255));
-
+	
 	orb::Orbis* orbis = orb::Orbis::instance();
 	while (orbis->isLevelRunning())
 	{
 		static int cnt = 0; cnt++;
 		if (cnt > 3) 
-			orbis->queueLevel(level2);
+			orbis->queueLevel(app::Level1::run);
 
 		orb::Log().message() << "level1::update" << std::endl;
 		orbis->updateFrame();
@@ -33,7 +24,7 @@ void level1()
 
 int main(int argc, char* args[])
 {
-	level1();
+	level0();
 	while (orb::Orbis::instance()->isLevelQueued())
 		orb::Orbis::instance()->runLevel();
 
